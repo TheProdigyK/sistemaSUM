6f387d09f477fcf0438021d1576e8f5f9f775a8f
TheProdigy <luisale0410@gmail.com>
2022-05-11 16:12:52 -0400

    active process bug

--- src/app/modules/admin/dashboard/active-process/active-process.component.ts
+++ src/app/modules/admin/dashboard/active-process/active-process.component.ts
@@ -9,6 +9,16 @@ import { MatTableDataSource } from '@angular/material/table';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
 
+export interface Proceso{
+  id_proceso: string;
+  nombre: string;
+  fecha_inicio: string;
+  id_sumariante: string;
+}
+
+const ELEMENT_DATA2: Proceso[] = [
+  {id_proceso: 'p1', nombre:'n1', fecha_inicio:'10/05/22', id_sumariante:'sum1'}
+];
 @Component({
   selector: 'app-active-process',
   templateUrl: './active-process.component.html',
@@ -17,11 +27,13 @@ import { HttpClient } from '@angular/common/http';
 export class ActiveProcessComponent implements OnInit {
   @ViewChild('paginator') paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @Input() user_name = 'USER_2';
+  @Input() user_name = 'U1';
 
   displayedColumns: string[] = ['id_proceso', 'nombre', 'fecha_inicio', 'id_sumariante', 'button-edit'];
-  dataSource!: MatTableDataSource<any>;
+  dataSource!: MatTableDataSource<Proceso>;
   data:any;
+  ELEMENT_DATA: Proceso[] = [];
+  
 
   constructor(
     private _liveAnnouncer:LiveAnnouncer,
@@ -32,13 +44,18 @@ export class ActiveProcessComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.user_name)
     const baseUrl = 'http://localhost:8080/process'
+    this.dataSource = new MatTableDataSource(ELEMENT_DATA2);
     
     this.http.get(`${baseUrl}/${this.user_name}`).subscribe(data => {
       this.data = data;
-      console.log(this.data)
+      //console.log(this.data)
+      this.saveDataProcess(data)
+      console.log(this.ELEMENT_DATA)
      
-      this.dataSource = new MatTableDataSource(this.data);
+      // this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
       this.dataSource.paginator = this.paginator
+      this.dataSource.sort = this.sort
+
       
     
       }, error => console.error(error));
@@ -46,6 +63,26 @@ export class ActiveProcessComponent implements OnInit {
   
   }
   
+  saveDataProcess(data: any){
+    var process: Proceso = {
+      id_proceso: '', 
+      nombre:'', 
+      fecha_inicio:'', 
+      id_sumariante:''
+    }
+
+    for (let p in data){
+      process.fecha_inicio = data[p]['fecha_inicio'];
+      process.id_proceso = data[p]['id_proceso'];
+      process.nombre = data[p]['nombre'];
+      process.id_sumariante = 'SUM1';
+
+      this.ELEMENT_DATA.push(process)
+      
+    }
+
+  }
+
   public pressed(){
     const dialogRef = this.dialog.open(EditProcessDialogComponent, {
       width: '1000px',
