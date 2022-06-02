2aa883c303acf9fed3b257dd23d30e7a708439f6
TheProdigy <luisale0410@gmail.com>
2022-06-02 01:07:30 -0400

    Sumariado dialog optimizacion

--- src/app/modules/admin/dashboard/add-process/sumariado-dialog/sumariado-dialog.component.ts
+++ src/app/modules/admin/dashboard/add-process/sumariado-dialog/sumariado-dialog.component.ts
@@ -6,38 +6,12 @@ import { HttpClient } from '@angular/common/http';
 import { FormControl, Validators } from '@angular/forms';
 import { Sumariado } from 'src/app/models/sumariado';
 
-
-export class SumariadoCheck{
-  id_sumariado: number;
-  checked: Boolean;
-
-  constructor(id_sumariado: number){
-    this.id_sumariado = id_sumariado;
-    this.checked = false;
-  }
-}
-const ELEMENT_DATA: SumariadoL[] = [
-  {numero: 1, nombre: 'Sumariado 1', fecha: '20-05-2022'},
-  {numero: 2, nombre: 'Sumariado 2', fecha: '19-05-2022'},
-  {numero: 3, nombre: 'Sumariado 3', fecha: '18-05-2022'},
-  {numero: 4, nombre: 'Sumariado 4', fecha: '17-05-2022'},
-  {numero: 5, nombre: 'Sumariado 5', fecha: '16-05-2022'}
-];
-
 @Component({
   selector: 'app-sumariado-dialog',
   templateUrl: './sumariado-dialog.component.html',
   styleUrls: ['./sumariado-dialog.component.scss']
 })
 export class SumariadoDialogComponent implements OnInit {
-
-  //TABLE
-  displayedColumns: string[] = ['id', 'name', 'date', 'actions'];
-  dataSource = ELEMENT_DATA;
-
-  //CHECK BOX LIST
-  sumariadoCheckedList: SumariadoCheck[] = []
-  checked = [];
 
   value=''
 
@@ -51,7 +25,6 @@ export class SumariadoDialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<SumariadoDialogComponent>,
-    private http: HttpClient,
     private sumariadoServices: SumariadoService
 
   ) { }
